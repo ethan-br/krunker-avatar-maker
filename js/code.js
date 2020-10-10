22b9955df946db659f9bb9538a65231df328cb38
Haptikal <haptikal@hotmailcom>
2020-10-10 12:50:09 -0400

    Alpha Version 0.2
    Fixed some assets
    Fixed not using right font
    Added skin selection

--- js/code.js
+++ js/code.js
@@ -1,1 +1,56 @@
-javascript code
+function skin() {
+	var skinValue = document.getElementById("skinSelect");
+	if (skinValue.value == "tan"){
+		document.getElementById("imageSkin").src = "assets/skin/tan.png"}
+	else if (skinValue.value == "sunburnt"){
+		document.getElementById("imageSkin").src = "assets/skin/sunburnt.png"}
+	else if (skinValue.value == "black"){
+		document.getElementById("imageSkin").src = "assets/skin/black.png"}
+	else if (skinValue.value == "alien"){
+		document.getElementById("imageSkin").src = "assets/skin/alien.png"}
+	else if (skinValue.value == "infected"){
+		document.getElementById("imageSkin").src = "assets/skin/infected.png"}
+	else{
+		document.getElementById("imageSkin").src = "assets/skin/white.png"}
+	console.log("Hair type changed to " + skinValue.value + ".")
+}
+
+function hairType() {
+	console.log("hair type changed")
+}
+
+function hairColor() {
+	console.log("hair color changed")
+}
+
+function backHair() {
+	console.log("back hair changed")
+}
+
+function beardType() {
+	console.log("beard type changed")
+}
+
+function beardColor() {
+	console.log("beard color changed")
+}
+
+function hatType() {
+	console.log("hat type changed")
+}
+
+function hatColor() {
+	console.log("hat color changed")
+}
+
+function clothesType() {
+	console.log("clothes type changed")
+}
+
+function clothesColor() {
+	console.log("clothes color changed")
+}
+
+function accentColor() {
+	console.log("accent changed")
+}
